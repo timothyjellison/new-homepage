fe865dbf331f63486dd4363efde7e7a7cedb3107
Timothy Ellison <timothyellison85@gmail.com>
2018-09-20 08:10:05 -0400

    use cloudinary for images

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ class BlogIndex extends React.Component {
       <div>
         <Helmet title={siteTitle} />
         <div className="home-page">
-          <img className="profile-pic" src="https://www.timothyellison.com/wp-content/uploads/2018/08/profile-pic-300x300.jpg"></img>
+          <img className="profile-pic" src="https://res.cloudinary.com/dzwa7qhj1/image/upload/c_scale,e_grayscale,w_300/v1523146834/fullsizeoutput_e44.webp"></img>
           <p>Hi! I’m Tim, a web developer living in Philadelphia, PA. I work at Comcast building web apps, mostly with React and Node. If you’ve ever used Comcast’s web apps to upgrade your cable package or move your service to a new house, I probably helped build those pages.</p>
           <p>I’m available for consultation and small-scale projects, so if you’d like to get in touch I’d love to hear from you by email or on Twitter.</p>
         </div>
