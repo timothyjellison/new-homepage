e3caf59abb3695f652375e2cde284fe5fc97e7d9
Timothy Ellison <timothyellison85@gmail.com>
2018-09-21 08:13:26 -0400

    provide fallback from webp

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,18 @@ import './index.css';
 
 class BlogIndex extends React.Component {
   render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
+    const siteTitle = get(this, 'props.data.site.siteMetadata.title');
+    const profileUrlStub = "https://res.cloudinary.com/dzwa7qhj1/image/upload/c_scale,e_grayscale,w_300/v1523146834/fullsizeoutput_e44";
 
     return (
       <div>
         <Helmet title={siteTitle} />
         <div className="home-page">
-          <img className="profile-pic" src="https://res.cloudinary.com/dzwa7qhj1/image/upload/c_scale,e_grayscale,w_300/v1523146834/fullsizeoutput_e44.webp"></img>
+          <picture>
+            <source srcSet={`${profileUrlStub}.webp`} type="image/webp"/>
+            <source srcSet={`${profileUrlStub}.png`} type="image/png"/>
+            <img className="profile-pic" src={`${profileUrlStub}.png`} alt="Selfie of Tim Ellison"/>
+          </picture>
           <p>Hi! I work at Comcast building web apps. If you’ve ever used Comcast’s web apps to upgrade your cable package or move your service to a new house, you're welcome 😂.</p>
           <p>I’m available for consultation and small-scale projects, just <a href="https://twitter.com/timothyjellison" target="_blank">hit me up on Twitter</a>.</p>
         </div>
