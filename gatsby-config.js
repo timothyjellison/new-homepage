eade18fe209da98b9c0a19accb6182a6cd7b564c
Timothy Ellison <timothyellison85@gmail.com>
2018-09-22 14:11:58 -0400

    add google analytics tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,7 +63,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-52933192-2`,
       },
     },
     // `gatsby-plugin-feed`,
