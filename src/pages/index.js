17a9d5b611aff83af5d0bfba03062fe2becbff07
Timothy Ellison <timothyellison85@gmail.com>
2019-01-10 07:37:07 -0800

    update Netflix link to jobs page

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ class BlogIndex extends React.PureComponent {
             <source srcSet={`${profileUrlStub}.png`} type="image/png"/>
             <img className="profile-pic" src={`${profileUrlStub}.png`} alt="Selfie of Tim Ellison"/>
           </picture>
-          <p>Heya 👋 I'm Tim, a UI engineer at <a href="https://www.netflix.com" target="_blank" rel="noreferrer">Netflix</a>.</p>
+          <p>Heya 👋 I'm Tim, a UI engineer at <a href="https://jobs.netflix.com/" target="_blank" rel="noreferrer">Netflix</a>.</p>
           <p>I'm interested in linguistic/literary technologies like NLP 🗣️, Unicode 🔣, bots 🤖, and emoji 🚀, plus topics at the boundaries of the verbal like music 🎵 and poetry 📚.</p>
         </div>
       </div>
