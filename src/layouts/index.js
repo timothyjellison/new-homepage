8d0fd1fb0364ba3868838ccbdff3b88d215f09b8
Tim Ellison <tellison@netflix.com>
2019-03-27 20:39:58 -0700

    simplify a bit

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,29 +1,42 @@
 import React from 'react'
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
+import Link from 'gatsby-link'
 
 import './reset.css';
 import './global.css';
 import 'prismjs/themes/prism-tomorrow.css';
 import styles from './style.module.css';
 
-import Header from '../components/Header';
-import NavBar from '../components/NavBar';
-import Footer from '../components/Footer';
-
 class Template extends React.PureComponent {
   render() {
-    const { children } = this.props
-    const {author, role} = get(this, 'props.data.site.siteMetadata')
+    const {author, role} = get(this.props, 'data.site.siteMetadata')
+    const currentYear = new Date().getFullYear();
 
     return (
       <div>
         <Helmet title={`${author}: ${role}`} />
         <div className={styles.layout}>
-          <Header author={author} role={role}/>
-          <NavBar />
-          <main className={styles.mainContent}>{children()}</main>
-          <Footer />
+
+          <header className={styles.header}>
+            <Link to={'/'} className={styles.link}>
+              <h1 className={styles.name}>{author}</h1>
+              <h2>{role}</h2>
+            </Link>
+          </header>
+
+          <nav className={styles.nav}>
+            <Link to="/" className={styles.link}>About</Link>
+            <Link to="/blog" className={styles.link}>Blog</Link>
+          </nav>
+
+          <main className={styles.mainContent}>
+            {this.props.children()}
+          </main>
+
+          <footer className={styles.footer}>
+            Tim Ellison &copy; {currentYear}
+          </footer>
         </div>
       </div>
     )
