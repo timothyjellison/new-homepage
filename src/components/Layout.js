16257be2593095cadf99b77150734357dbb2abf0
Tim Ellison <timothyellison85@gmail.com>
2020-04-05 09:56:00 -0700

    add an admin page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,38 +4,70 @@ import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
 
 class Layout extends React.PureComponent {
+  componentDidMount() {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on('init', user => {
+        if (!user) {
+          window.netlifyIdentity.on('login', () => {
+            document.location.href = '/admin/'
+          })
+        }
+      })
+    }
+  }
+
   render() {
-    const {pathname} = get(this.props, 'location');
-    const currentYear = new Date().getFullYear();
+    const { pathname } = get(this.props, 'location')
+    const currentYear = new Date().getFullYear()
 
     return (
       <>
-        <Helmet title="Tim Ellison: Front-End Web Developer" />
+        <Helmet title="Tim Ellison: Front-End Web Developer">
+          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+        </Helmet>
         <div className="layout">
-
           <header className="header grid">
             <Link to={'/'} className="link">
-              <h1 className={"name"}>Tim Ellison</h1>
+              <h1 className={'name'}>Tim Ellison</h1>
               <h2>Front-End Web Developer</h2>
             </Link>
           </header>
 
           <nav className="nav grid">
             <div className="navItems">
-              <Link to="/" className={pathname === '/' ? "linkSelected" : "link"}>Home</Link>
-              <Link to="/articles" className={(pathname.includes('posts') || pathname.includes('/articles'))  ? "linkSelected" : "link"}>Articles</Link>
-              <Link to="/faves" className={pathname.includes('faves') || pathname === '/faves' ? "linkSelected" : "link"}>Faves</Link>
+              <Link
+                to="/"
+                className={pathname === '/' ? 'linkSelected' : 'link'}
+              >
+                Home
+              </Link>
+              <Link
+                to="/articles"
+                className={
+                  pathname.includes('posts') || pathname.includes('/articles')
+                    ? 'linkSelected'
+                    : 'link'
+                }
+              >
+                Articles
+              </Link>
+              <Link
+                to="/faves"
+                className={
+                  pathname.includes('faves') || pathname === '/faves'
+                    ? 'linkSelected'
+                    : 'link'
+                }
+              >
+                Faves
+              </Link>
               {/* <Link to="/music" className={pathname === '/music' ? "linkSelected" : "link"}>Music</Link> */}
             </div>
           </nav>
 
-          <main className={"mainContent grid"}>
-            {this.props.children}
-          </main>
+          <main className={'mainContent grid'}>{this.props.children}</main>
 
-          <footer className={"footer"}>
-            Tim Ellison &copy; {currentYear}
-          </footer>
+          <footer className={'footer'}>Tim Ellison &copy; {currentYear}</footer>
         </div>
       </>
     )
