b3e2ee480491ad5f77aef845bd1174d72100be3d
Tim Ellison <timothyellison85@gmail.com>
2020-07-12 13:11:01 -0700

    simplify about me section (#12)

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -2,22 +2,10 @@ import React from 'react'
 import Link from 'gatsby-link'
 import styles from './index.module.css'
 
-const LilHook = ({ href, children }) => (
-  <a href={href} target="_blank" rel="noreferrer noopener">
-    {children}
-  </a>
-)
-
 export default () => (
   <nav className={styles.navBar}>
     <div>
-      <Link to={'/'}>Timothy Ellison</Link>
-    </div>
-    <div className={styles.externalLinks}>
-      <LilHook href="https://github.com/timothyjellison">Github</LilHook>
-      <LilHook href="https://www.linkedin.com/in/timothyjellison/">
-        LinkedIn
-      </LilHook>
+      <Link to={'/'}>Home</Link>
     </div>
   </nav>
 )
