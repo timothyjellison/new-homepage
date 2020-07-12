4941af513da8582dc19d17ef9a3979b61ca42038
Tim Ellison <timothyellison85@gmail.com>
2020-07-12 14:09:56 -0700

    add some color (#13)

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,4 +1,10 @@
 import Typography from 'typography'
 import theme from 'typography-theme-parnassus'
 
+theme.overrideThemeStyles = () => ({
+  a: {
+    color: '#e8505b',
+  },
+})
+
 export default new Typography(theme)
